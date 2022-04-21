bb1ab226f22e02f2331c23fc713f2701395ee44a
felistus <felistusobieze@gmail.com>
2022-04-21 14:01:45 +0100

    modified chat-screen to channels

--- pages/user-profile.tsx
+++ pages/user-profile.tsx
@@ -1,88 +1,62 @@
-<<<<<<< HEAD
-import Navigation from "../components/Navigation"
-import Script from "next/script"
-import { getSession } from "next-auth/react"
-import { useRef, useState, useEffect } from "react"
-import { updateUserProfile } from "../services/profile"
-import CloudinaryUploadWidget from "../components/CloudinaryWidget"
-=======
 import Navigation from "../components/Navigation";
 import Script from "next/script";
 import { getSession } from "next-auth/react";
 import { useState } from "react";
 import { updateUserProfile } from "../services/profile";
 import CloudinaryUploadWidget from "../components/CloudinaryWidget";
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
 
 export interface Props {
-  user: User
+  user: User;
 }
 
 export default function EditProfile({ user }: Props) {
-<<<<<<< HEAD
-  const [uploadPhoto, setUploadPhoto] = useState<File>()
-  const [editInfo, setEditInfo] = useState(false)
-  const [userPhoto, setUserPhoto] = useState(user.image)
-  const [userBio, setUserBio] = useState(user.bio)
-  const imageRef = useRef<any>(null)
-=======
   const [editInfo, setEditInfo] = useState(false);
   const [userPhoto, setUserPhoto] = useState(user.image);
   const [userBio, setUserBio] = useState(user.bio);
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
 
   function editUserDetails() {
-    setEditInfo(true)
+    setEditInfo(true);
     if (editInfo) {
-      setEditInfo(false)
+      setEditInfo(false);
     }
   }
   function changeBioInfo(event: any) {
-    setUserBio(event.target.value)
+    setUserBio(event.target.value);
   }
-<<<<<<< HEAD
-  function handlePhotoUpload(event: any) {
-    const uploadedPhoto = event.target.files[0]
-    setUploadPhoto(uploadedPhoto)
-    const imgUrl = URL.createObjectURL(uploadedPhoto)
-    setUserPhoto(imgUrl)
-  }
-=======
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
 
   async function handleProfileUpdate() {
-    if (!userBio) return
-    const { data, error } = await updateUserProfile(userBio)
+    if (!userBio) return;
+    const { data, error } = await updateUserProfile(userBio);
     if (data) {
-<<<<<<< HEAD
-      console.log(data)
-      setUserBio(data.bio)
-=======
       setUserPhoto(data.image);
       setUserBio(data.bio);
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
     }
-    setEditInfo(false)
+    setEditInfo(false);
   }
 
   function handleButtonToggle() {
     if (editInfo) {
-      handleProfileUpdate()
+      handleProfileUpdate();
     } else {
-      editUserDetails()
+      editUserDetails();
     }
   }
 
   return (
     <div className="bg-white-offwhite min-h-screen h-full">
-      <Script src="https://widget.cloudinary.com/v2.0/global/all.js" strategy="beforeInteractive" />
+      <Script
+        src="https://widget.cloudinary.com/v2.0/global/all.js"
+        strategy="beforeInteractive"
+      />
       <div className="px-4 py-2 ">
         <Navigation name={user.name} image={userPhoto} />
       </div>
       <section className="pt-12 md:px-4 md:pb-24 ">
         <div className="mb-6 text-center md:mb-11">
           <p className="mb-2 text-4xl text-black">Personal Info</p>
-          <p className="text-[18px] text-black ">Basic info, like your name and photo</p>
+          <p className="text-[18px] text-black ">
+            Basic info, like your name and photo
+          </p>
         </div>
         <div className="md:border-2 md:border-white-light md:w-[700px] md:mx-auto md:rounded-xl">
           <div className="px-[22px] md:px-[50px] py-7 flex items-center justify-between border-b-[1px] border-b-white-cream ">
@@ -92,41 +66,46 @@ export default function EditProfile({ user }: Props) {
                 Some info maybe visible to other people
               </p>
             </div>
-            <div onClick={handleButtonToggle} className="border-[1px] border-[#828282] w-[95px] rounded-xl ">
-              <button className="w-full p-2 text-[#828282] font-medium text-base ">{editInfo ? "Save" : "Edit"}</button>
+            <div
+              onClick={handleButtonToggle}
+              className="border-[1px] border-[#828282] w-[95px] rounded-xl "
+            >
+              <button className="w-full p-2 text-[#828282] font-medium text-base ">
+                {editInfo ? "Save" : "Edit"}
+              </button>
             </div>
           </div>
           <div className="px-[22px] md:px-[50px] py-6 flex items-center space-x-40 border-b-[1px] border-b-[#D3D3D3] ">
-            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px] ">Photo</span>
+            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px] ">
+              Photo
+            </span>
             <div className="flex items-center justify-center w-[72px] h-[72px] rounded-lg overflow-hidden border-2 relative">
-<<<<<<< HEAD
-              <img className="block w-full h-auto" src={userPhoto} alt="user image" />
-=======
               <img
                 className="block w-full h-full"
                 src={userPhoto}
                 alt="user image"
               />
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
               <div className="absolute bg-transparent cursor-pointer">
-                {editInfo ? <CloudinaryUploadWidget update={setUserPhoto} /> : null}
+                {editInfo ? (
+                  <CloudinaryUploadWidget update={setUserPhoto} />
+                ) : null}
               </div>
-<<<<<<< HEAD
-              <input ref={imageRef} type="file" onChange={handlePhotoUpload} hidden />
-=======
->>>>>>> 1a64a49d86272c1bfb9001af16a630b8a798dfd1
             </div>
           </div>
 
           <div className="px-[22px] md:px-[50px] py-6 flex items-center space-x-40 border-b-[1px] border-b-[#D3D3D3] cursor-not-allowed  ">
-            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px]  ">Name</span>
+            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px]  ">
+              Name
+            </span>
             <div className="font-medium text-[#333333] text-[18px] ">
               <p className="">{user.name}</p>
             </div>
           </div>
 
           <div className="px-[22px] md:px-[50px] py-6 flex items-center md:space-x-40 space-x-16 border-b-[1px] border-b-[#D3D3D3] ">
-            <div className="text-[#BDBDBD] uppercase text-[13px] font-medium  w-[43px]  ">Bio</div>
+            <div className="text-[#BDBDBD] uppercase text-[13px] font-medium  w-[43px]  ">
+              Bio
+            </div>
             <div className="flex-1 ">
               {editInfo ? (
                 <textarea
@@ -140,36 +119,42 @@ export default function EditProfile({ user }: Props) {
                   onChange={changeBioInfo}
                 ></textarea>
               ) : (
-                <p className="font-medium text-[#333333] text-base cursor-not-allowed ">{userBio}</p>
+                <p className="font-medium text-[#333333] text-base cursor-not-allowed ">
+                  {userBio}
+                </p>
               )}
             </div>
           </div>
 
           <div className="px-[22px] md:px-[50px] py-6 flex items-center md:space-x-40 space-x-16 border-b-[1px] border-b-[#D3D3D3] md:border-b-0 cursor-not-allowed   ">
-            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px]  ">Email</span>
+            <span className="text-[#BDBDBD] uppercase text-[13px] font-medium w-[43px]  ">
+              Email
+            </span>
             <div>
-              <p className="font-medium text-[#333333] text-[18px] cursor-not-allowed ">{user.email}</p>
+              <p className="font-medium text-[#333333] text-[18px] cursor-not-allowed ">
+                {user.email}
+              </p>
             </div>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(ctx: any) {
-  const session = await getSession(ctx)
+  const session = await getSession(ctx);
   if (session && session.user) {
     return {
       props: {
         user: session.user,
       },
-    }
+    };
   }
   return {
     redirect: {
       permanent: false,
       destination: "/",
     },
-  }
+  };
 }
