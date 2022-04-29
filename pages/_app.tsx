04ae73a0475752ffd63800cc4be8f466a33cad0d
Akuma Isaac Akuma <akumaisaacakuma.com>
2022-04-29 20:02:42 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,9 +62,8 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   useEffect((): any => {
     if (pageProps.user) {
       if (!socket) {
-        newsocket = io(location.origin, {
-          path: "/api/socket",
-        }).connect()
+        axios.get("/api/socket")
+        newsocket = io().connect()
         setSocket(newsocket)
       }
     }
