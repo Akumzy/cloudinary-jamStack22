da9505992a7e0b3890da749965d0843cc1b8500c
princewhyte dabotubo <princewhyte02@gmail.com>
2022-04-29 23:07:35 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,7 +47,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   useEffect((): any => {
     if (pageProps.user) {
       if (!socket) {
-        axios.get("/api/socket").finally(() => {
+        axios.get("/api/socket").then(() => {
           newsocket = io()
           setSocket(newsocket)
         })
